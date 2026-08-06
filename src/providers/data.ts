34bcdbfe53503b82284dcbda3e7a3b4b82493867
Tapas Ranjan Jena <tapasranjan1174@gmail.com>
2026-08-06 13:39:27 +0530

    add some error handlers

--- src/providers/data.ts
+++ src/providers/data.ts
@@ -1,9 +1,26 @@
 import {createDataProvider, CreateDataProviderOptions} from "@refinedev/rest";
 import {BACKEND_BASE_URL} from "@/constants";
 import {ListResponse} from "@/types";
+import {HttpError} from "@refinedev/core"
 
 if(!BACKEND_BASE_URL){
     throw new Error('BACKEND_BASE_URL is not configured.please set VITE_BACKEND_BASE_URL in your .env file.')
+}
+
+const builtHttpError = async (response:Response) : Promise<HttpError> => {
+    let message='Request failed';
+
+    try{
+        const payload =(await response.json()) as {message?: string};
+        if(payload?.message) message = payload.message;
+    }catch{
+        // ignore error and keep default message
+    
+    }
+    return{
+        message,
+        statusCode:response.status
+    }
 }
 
 const options: CreateDataProviderOptions = {
@@ -31,12 +48,15 @@ const options: CreateDataProviderOptions = {
         },
 
         mapResponse: async (response) => {
+            if(!response.ok) throw await builtHttpError(response);
+
             const payload: ListResponse = await response.clone().json();
 
             return payload.data ?? [];
         },
 
         getTotalCount: async (response) => {
+            if(!response.ok) throw await builtHttpError(response);
             const payload: ListResponse = await response.clone().json();
 
             return payload.pagination?.total ?? payload.data?.length ?? 0;
